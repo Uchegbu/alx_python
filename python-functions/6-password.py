28b8c12541b61027fc53a498538ac1e7218ba8fb
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-05-16 09:38:35 +0100

    6-password.py

---
+++ python-functions/6-password.py
@@ -0,0 +1,12 @@
+def validate_password(password):
+    if (
+        len(password) >= 8
+        and any(char.isupper() for  char in password)
+        and any(char.islower() for  char in password)
+        and any(char.isdigit() for  char in password)
+        and ' ' not in password
+    ):
+        return True 
+    
+    else:
+        return False   
