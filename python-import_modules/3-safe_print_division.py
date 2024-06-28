0bf9b272508b78bfaf85d3f0ef67dd9c031a6903
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-06-28 09:00:54 +0100

    3-safe_print_division.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,10 +4,10 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result  = None
     finally:
-        print("inside result: {}".format(result))
-        print("{}/{}  = {}".format(a,b, result))
+        print("inside result: {}.0".format(result))
+        print("{}/{} = {}.0".format(a,b, result))
     return result
 
 
 if __name__ == "__main__":
-    safe_print_division(10,0)
+    safe_print_division(10,2)
