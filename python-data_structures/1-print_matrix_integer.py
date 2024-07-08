f275a8936babd66e931c300fc7459f50cf924120
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-07-08 18:50:16 +0100

    1-print_matrix_integer.py

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,10 +1,12 @@
 def print_matrix_integer(matrix=[[]]):
-    result = ""
-    for row in matrix:
-        for elem in row:
-            result += "{:d} ".format(elem)
-        result += "\n"
-    return result
+    """Print a matrix of integers."""
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
+
+        print("")
 
 matrix = [
 	[1, 2, 3],
@@ -12,6 +14,5 @@ matrix = [
 	[7, 8, 9]
 ]
 
-
 if __name__ == "__main__":
-    print(print_matrix_integer(matrix))
+    (print_matrix_integer(matrix))
