28734d0ddae817d2997b110127b7b45ae441e45c
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-07-18 10:42:23 +0100

    0-square_matrix_simple.py

---
+++ python-data_structures/0-square_matrix_simple.py
@@ -0,0 +1,20 @@
+def square_matrix_simple(matrix=[]):
+    if not matrix or not matrix[0]:
+        return["matrix is empty"]
+    squared_matrix = []
+    for row in matrix:
+        squared_row = []
+        for elem in row:
+            squared_row.append(elem**2)
+        squared_matrix.append(squared_row)
+    return squared_matrix
+
+matrix = [
+    [1, 2, 3],
+    [2, 3, 4],
+    [3, 4, 5]
+]
+
+
+if __name__ == "__main__":
+    print(square_matrix_simple(matrix))
