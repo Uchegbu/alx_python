13d5ac00882d86c6c8eaeb0f9144fab168534961
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-07-19 08:45:34 +0100

    2-update_dictionary.py

---
+++ python-more_data_structures/2-update_dictionary.py
@@ -0,0 +1,10 @@
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    return a_dictionary
+
+a_dictionary = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+a_dictionary['b'] = 4
+
+if __name__ == "__main__":
+   print(update_dictionary(a_dictionary, 'b', 4))
