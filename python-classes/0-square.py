9671e77f88182ab132730735a7f7863cc69845e6
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-08-20 12:47:57 +0100

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,5 @@
+""" create a class called square"""
+
+class square:
+    def __init__(self, size) -> None: # the __init__ is receiving an argument "size"
+        self.__size = size  # __size is a private instance attribute
