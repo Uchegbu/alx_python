b268494140b95fa18a1e92621a56f831bfb6ed46
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-10-01 16:02:59 +0100

    1- is_kind_of_class

---
+++ python-inheritance/1- is_kind_of_class.py
@@ -0,0 +1,37 @@
+"""
+this script write a function that returns true if the object is an instance of,or if the object
+is an instance of a class that inherited from, the specified class,otherwise false.
+"""
+
+class Animal:
+    """"
+    this is the parent class
+    """
+    def __init__(self): #no parameter pass
+        pass
+
+class a_class(Animal):
+    """
+    this class is a subclass of Animal and will serve as the specified class
+    """
+    def __init__(self): #no parameter passed
+        pass
+
+class Cat(a_class):
+    """
+    this is subclass of Dog and it is inheriting from dog
+    """
+    def __init__(self): #no parameter is passed
+        pass
+
+#creating an instance of Dog
+obj = a_class()
+my_cat = Cat()
+
+"""
+creating a funcion that return true when is an instance or inheriting from the specifird class
+"""
+def is_kind_of_class(obj,a_class):
+    return isinstance(obj,a_class)
+
+print(is_kind_of_class(my_cat,a_class))
