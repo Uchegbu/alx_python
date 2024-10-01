66e0a5735ccdf8689d324abfe38dfc88f6b3ff55
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-10-01 13:32:46 +0100

    is the same class

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
         return result
-
+    
 
 if __name__ == "__main__":
     safe_print_division(10,6)
---
+++ python-inheritance/0-is_same_class.py
@@ -0,0 +1,28 @@
+"""
+this script will create A function that returns true if the object is exactly an instance of the specified class and othwerwise false,i.e i'm working on inheritance
+"""
+
+class Animal:
+    """
+    this class "animal" will serve as the parent class or super class. 
+    """
+    def __init__(self): # no parameter is passed,it is empty.
+        pass
+
+class a_class(Animal):
+    """
+    this class "a_class" is the subclass of "Animal",a_class is inheriting from animal.
+    """
+    def __init__(self): # no parameter is passed,it is empty.
+        pass
+
+#creating an instance of the class a_class
+obj = a_class()
+
+"""
+a function that return true if an object is exactly the same of a class and false otherwise.
+"""
+def is_same_class(obj, a_class):
+    return type(obj) == a_class
+    
+print(is_same_class(obj, a_class))
