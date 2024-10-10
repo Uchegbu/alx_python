143224a0ce3a2363fdd0a73265afdf26bd7ac05a
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-10-10 20:36:13 +0100

    3-base_geometry

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,9 @@
+"""
+in this script, i will be writing a empty class called "BaseGeometry" 
+"""
+
+class BaseGeometry: #empty class
+    pass
+
+#creating an instance of the class
+obj = BaseGeometry()
