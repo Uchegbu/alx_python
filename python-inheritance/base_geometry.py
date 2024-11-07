3f128212aeb004015aee60eb5a8e72c9112303be
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-11-07 05:15:53 +0100

    rectangle_7

--- python-inheritance/base_geometry.py
+++ python-inheritance/base_geometry.py
@@ -27,7 +27,8 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-#creatint try-except block for area and integer validator
+        
+#creating try-except block for area and integer validator
 def area(obj):
     try:
         return(obj.area())
---
+++ python-inheritance/rectangle_7.py
@@ -0,0 +1,31 @@
+"""
+This script is a full rectangle. This python script will be inheriting from the class BaseGeometry which i will be importing as module. 
+this script will have attribute i.e width and height and methods.The width and height will be validated as whether be postive integer
+by integer_validator from the BaseGeometry.note: width and height are private attribute. 
+"""
+
+from base_geometry import BaseGeometry
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        try:
+            super().integer_validator("width", width)
+            super().integer_validator("height", height)
+            self.__width = width
+            self.__height = height
+        except ValueError as e:
+            raise ValueError("invalid rectangle dimension:{}".format(e))
+
+    def __str__(self) -> str:
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
+    
+if __name__ == "__main__":  
+    try: 
+        obj = Rectangle(7,7)
+        print(obj)
+        print(obj.area())
+    except ValueError as e:
+        print(e)
