a3d63a9a1ec1b0e1eb66fcd5c88f9ee04f199b5a
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-11-21 17:37:28 +0100

    models/base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,3 +1,7 @@
+"""
+This class will be the “base” of all other classes in this project. 
+The goal of it is to manage id attribute in all your future classes and to avoid duplicating the same code (by extension, same bugs)
+"""
 class Base:
     """
      Base class for all objects.
