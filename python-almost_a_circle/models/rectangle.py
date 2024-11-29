b705d5b6bd741bbfc45f92c226b98e9a340b8909
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-11-29 14:03:29 +0100

    commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,15 +1,16 @@
-from base import Base
+from models.base import Base
 
 class Rectangle(Base):
     """
     contains four private instance attributes with its own public getters and setters
     """
     def __init__(self, width, height, x=0, y=0, id=None): 
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super().__init__(id)
+        
 
     #creating getter and setter for width
     @property
